6a39b7e3e1807dac0aa7b4130252eda4231952c1
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2022-03-17 00:57:52 +0530

    resposive layout and firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone_flutter/responsive/mobile_screen_layout.dart';
+import 'package:insta_clone_flutter/responsive/responsive_layout.dart';
+import 'package:insta_clone_flutter/responsive/web_screen_layout.dart';
 import 'package:insta_clone_flutter/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Instagram clone flutter.',
+      title: 'Instagram clone',
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Instagram clone.'),
-        ),
+      home: const ResponsiveLayout(
+        webScreenLayout: WebScreenLayout(),
+        mobileScreenLayout: MobileScreenLayout(),
       ),
     );
   }
